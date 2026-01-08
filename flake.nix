82020436754e254ced9c123acea071fe462a1dc4
Asher <146795551+ALH477@users.noreply.github.com>
2026-01-07 19:05:55 -0800

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     fw-fanctrl.url = "github:TamtamHero/fw-fanctrl/packaging/nix";
     demod-ip-blocker.url = "git+https://github.com/ALH477/DeMoD-IP-Blocker.git";
-    minecraft.url = "github:ALH477/NixOS-MineCraft";
+	minecraft.url = "github:ALH477/NixOS-MineCraft";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, determinate, nixos-hardware, fw-fanctrl, demod-ip-blocker, minecraft ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, determinate, nixos-hardware, fw-fanctrl, demod-ip-blocker, minecraft, ... }@inputs:
   let
     system = "x86_64-linux";
   in {
@@ -23,7 +23,6 @@
         nixos-hardware.nixosModules.framework-16-7040-amd
         fw-fanctrl.nixosModules.default
         demod-ip-blocker.nixosModules.default
-        minecraft.nixosModules.default
         
         # Point to your external configuration file
         ./configuration.nix
