7a0ebefa56f59e3a1d477c5e8deab5fea1932b31
Asher <146795551+ALH477@users.noreply.github.com>
2026-01-07 19:57:54 -0800

    Add files via upload

---
+++ modules/cachyos-bore-kernel.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, lib, inputs, ... }:
+
+let
+  kernelVersion = "6.18.3";
+  kernelDir = let parts = builtins.splitVersion kernelVersion; in "${builtins.elemAt parts 0}.${builtins.elemAt parts 1}";
+  linux_src = pkgs.fetchurl {
+    url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${kernelVersion}.tar.xz";
+    sha256 = "eoh5FnuJxLrgd9bznE8hMHafBdva0qrZFK2rmvt9f5o=";  # Correct hash for linux-6.18.3.tar.xz (computed from actual download as of Jan 2026).
+  };
+  boreConfig = "${inputs.linux-cachyos}/linux-cachyos-bore/config";
+  kernelPatches = [
+    {
+      name = "cachyos-base-all";
+      patch = pkgs.fetchpatch {
+        url = "https://raw.githubusercontent.com/cachyos/kernel-patches/master/${kernelDir}/all/0001-cachyos-base-all.patch";
+        sha256 = "043kcszx2hnfinq6j92zzas4ghzqk42ah8hr73cnrzksp8kjqjl9";
+      };
+    }
+    {
+      name = "bore-cachy";
+      patch = pkgs.fetchpatch {
+        url = "https://raw.githubusercontent.com/cachyos/kernel-patches/master/${kernelDir}/sched/0001-bore-cachy.patch";
+        sha256 = "10xl6vakjs3njmx1kgacs4zypyz4smcm8180h8zf0kiprk9753gq";
+      };
+    }
+  ];
+  customKernel = (pkgs.linuxManualConfig {
+    inherit (pkgs) stdenv;
+    version = "${kernelVersion}-cachyos-bore";
+    src = linux_src;
+    configfile = boreConfig;
+    allowImportFromDerivation = true;
+    kernelPatches = kernelPatches;
+  }).overrideAttrs (oldAttrs: {
+    passthru = oldAttrs.passthru // {
+      features = { ia32Emulation = true; efiBootStub = true; };
+    };
+  });
+in {
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor customKernel);
+
+  # Ensure kvm-amd module is loaded (common for AMD virtualization; safe to add).
+  boot.kernelModules = lib.mkBefore [ "kvm-amd" ];
+
+  services.thermald.enable = true;
+  powerManagement.powertop.enable = true;
+}
